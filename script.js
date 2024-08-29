9eddab291df2934ce3942cdf1a1e1ccc66c26d63
kushal_tamang <kushaltamang0926@gmail.com>
2024-08-29 23:58:36 +0545

    editiing

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
 
+const toTop = document.querySelector(".back-top");
 
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 75) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 
